304035d514eca272f2b1ca0648540ac6384d5e39
Heni Mehta <88430391+heni2021@users.noreply.github.com>
2025-03-29 17:55:41 +0530

    Added code for reverse elements in array in java

---
+++ src/main/java/com/example/practice/codes/GeeksForGeeks/ReverseElementsInArray_Easy/ReverseElementsInArray_Java.java
@@ -0,0 +1,31 @@
+package com.example.practice.codes.GeeksForGeeks.ReverseElementsInArray_Easy;
+
+import java.util.Arrays;
+
+import static com.example.practice.codes.Utilities.ArrayUtilities_Java.scanIntArray;
+import static com.example.practice.codes.Utilities.ArrayUtilities_Java.swapElementInIntArrayAtIndexes;
+
+public class ReverseElementsInArray_Java {
+    public static void main(String[] args) {
+        int[] arr = scanIntArray();
+
+        reverseArrayInPlaceUsingBruteForce(arr);
+        System.out.println("Reversed Array after brute force : "+ Arrays.toString(arr));
+    }
+
+    /**
+     * Brute Force Approach - 2 Pointers Approach
+     * Take 2 pointers one at starting and one at ending index of the array.
+     * Keep swapping the elements till either both pointer crosses each other or at the same index.
+     * @timeComplexity : O(n)
+     * @spaceComplexity : O(1)
+     * */
+    public static void reverseArrayInPlaceUsingBruteForce(int[] arr){
+        int startIndex = 0, endIndex = arr.length-1;
+        while(startIndex<endIndex){
+            swapElementInIntArrayAtIndexes(arr, startIndex, endIndex);
+            startIndex++;
+            endIndex--;
+        }
+    }
+}
