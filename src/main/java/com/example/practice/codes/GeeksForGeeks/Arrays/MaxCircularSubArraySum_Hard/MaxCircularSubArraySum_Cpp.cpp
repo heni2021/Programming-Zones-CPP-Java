f3708c00988a0d8a4c2cc66a27fce87d7e9c7a93
Heni Mehta <88430391+heni2021@users.noreply.github.com>
2025-04-19 15:25:27 +0530

    Added code for max circular sub array in cpp

---
+++ src/main/java/com/example/practice/codes/GeeksForGeeks/Arrays/MaxCircularSubArraySum_Hard/MaxCircularSubArraySum_Cpp.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<math.h>
+#include<vector>
+using namespace std;
+
+/**
+ * Brute Force
+ * Find all possibilites and compute max sum of the circular subarray.
+ * @timeComplexity:O(n^2)
+ * @spaceComplexity:O(1)
+ * @param a
+ * @return
+ */
+int computeMaxSumBruteForce(vector<int> &a){
+    int maxSum = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        int currentSum = a[i];
+        maxSum = max(currentSum, maxSum);
+        int k = (i + 1 == a.size()) ? 0 : i + 1;
+        for (int j = k; j != i; j++)
+        {
+            if (i == 0 && j == a.size())
+            {
+                break;
+            }
+            if (j == a.size())
+            {
+                j = 0;
+            }
+            currentSum += a[j];
+            maxSum = max(currentSum, maxSum);
+        }
+    }
+
+    return maxSum;
+}
+
+/**
+ * Optimised Approach
+ * Find minimum sum and maximum sum in the array of a given subarray using kadane's algorithm.
+ * Also compute the total sum.
+ * Total sum - Minimum sum will obviously be the circular sum of the array. If total sum and minimum sum are same it means all the elements are negative and it means there is only one way to get max sum is to return maxsum obtained linerly.
+ * otherwise return maximum out of maxSum and circularSum.
+ * @timeComplexity:O(n)
+ * @spaceComplexity:O(1)
+ * @param a
+ * @return
+ */
+int computeMaxCircularSum(vector<int> &a){
+    int totalSum = 0;
+    int maxSum = a[0], minSum = a[0], circularSum;
+    int currMaxSum = 0, currMinSum = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        // kadane's algo to find max sum
+        currMaxSum = max(currMaxSum + a[i], a[i]);
+        maxSum =  max(maxSum, currMaxSum);
+
+        // Kadane's algo to find min sum
+        currMinSum =  min(currMinSum + a[i], a[i]);
+        minSum =  min(minSum, currMinSum);
+        totalSum += a[i];
+    }
+
+    circularSum = totalSum - minSum;
+
+    if (minSum == totalSum)
+    {
+        return maxSum;
+    }
+
+    return max(maxSum, circularSum);
+}
+
+int main(){
+    vector<int> a = {-7,32, -11, 21, 18, 35, -26, -17, 35, -12, -38, -33, 32, 16, 44, 11, -40, -21, 2, 27, -35, 21, -37, -12, 1};
+    int maxSumUsingBruteForce = computeMaxSumBruteForce(a);
+    cout << "Max Sum of circular array using brute force : " << maxSumUsingBruteForce << endl;
+
+    int maxSumUsingOptimisedApproach = computeMaxCircularSum(a);
+    cout << "Max Sum of circular array using optimised approach: " << maxSumUsingOptimisedApproach << endl;
+
+    return 0;
+}
+
+// 8 -8 9 -9 10 -11 12
+// 10 -3 -4 7 6 5 -4 -1
+// -1 40 -14 7 6 5 -4 -1
+// 13 7 12 0 -2 5 8
+// 12 -1
+// -7 32 -11 21 18 35 -26 -17 35 -12 -38 -33 32 16 44 11 -40 -21 2 27 -35 21 -37 -12 1
