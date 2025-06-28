fa7b664480b65dde3c28cee8d34cbbfd0ad17d69
Heni Mehta <88430391+heni2021@users.noreply.github.com>
2025-06-28 15:01:30 +0530

    Added code to print half pyramid pattern in cpp

--- src/main/java/com/example/practice/codes/Patterns/HalfPyramid/HalfPyramid_Java.java
+++ src/main/java/com/example/practice/codes/Patterns/HalfPyramid/HalfPyramid_Java.java
@@ -1,6 +1,15 @@
 package com.example.practice.codes.Patterns.HalfPyramid;
 
 import java.util.Scanner;
+
+/*
+    *
+    **
+    ***
+    ****
+    *****
+    ******
+*/
 public class HalfPyramid_Java {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
--- src/main/java/com/example/practice/codes/Patterns/HollowRectangle/HollowRectangle_Cpp.cpp
+++ src/main/java/com/example/practice/codes/Patterns/HollowRectangle/HollowRectangle_Cpp.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include<stdio.h>
 using namespace std;
-
+/*
+    ************
+    *          *
+    *          *
+    *          *
+    ************
+ */
 int main(){
     int n,m;
     cin >> n >> m;
