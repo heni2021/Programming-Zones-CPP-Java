94124695898446c25ace48ec152b182f19c0e126
heni2021 <mheni2021@gmail.com>
2026-05-17 16:02:21 +0530

    Removed unused import

--- src/main/java/com/example/practice/codes/CodingPatterns/TwoPointers/ThreeSum.java
+++ src/main/java/com/example/practice/codes/CodingPatterns/TwoPointers/ThreeSum.java
@@ -3,7 +3,6 @@ package com.example.practice.codes.CodingPatterns.TwoPointers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 import com.example.practice.codes.Utilities.ArrayUtilities_Java;
 
